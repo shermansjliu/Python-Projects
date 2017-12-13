74fe12ff349fe0e7677a029d426e556dcb8b780e
Sherman Liu <shermanliu03@gmail.com>
2017-12-13 23:22:04 +0800

    Bear Lab Complete

---
+++ Bear/Bear.py
@@ -0,0 +1,17 @@
+file_object = open("bears.dat","r")
+
+
+bearNum = 0
+for line in file_object:
+    lineArr = list(line)
+    for char in lineArr:
+        if char == "B":
+            bearNum += 1
+print(bearNum)
+
+
+
+#for eveery line in the file object
+#split the each line into an array of characters
+#for every character check if it is a B
+#increase bear count by 1
